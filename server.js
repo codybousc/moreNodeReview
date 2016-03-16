909f77baf982db28055daf1ab9f4cb91c64f6a26
Cody Bouscaren <codybousc@gmail.com>
2016-03-16 17:15:12 -0400

    Basic Structure Set

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080
