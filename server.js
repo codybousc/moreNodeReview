bc8fa57d962c4e736f812bc7e1b4bab75d57e618
Cody Bouscaren <codybousc@gmail.com>
2016-03-16 21:03:06 -0400

    Token Based Authentication Functioning

--- server.js
+++ server.js
@@ -91,7 +91,35 @@ apiRouter.post('/authenticate', function(req, res) {
 apiRouter.use(function(req, res, next) {
   //do logging
   console.log('Somebody just came to our app!');
-  next();
+
+  //check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  //decode token
+  if (token) {
+    //verify secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      }
+      else {
+        //if everything checks out, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+  else {
+    //if there's no token
+    //return an HTTP response of 403 (access forbidden) and an error message
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided, broda.'
+    });
+  }
 });
 
 //test route to make sure all is working properly
